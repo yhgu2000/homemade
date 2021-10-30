96663ba06a8837b4e9a5bc1199f701997b439898
Yuhao Gu <yhgu2000@outlook.com>
2021-10-30 16:45:56 +0800

    改用正则表达式解决click恼人的通配符问题

--- recode.py
+++ recode.py
@@ -1,12 +1,12 @@
-__version__ = "3.2 (2021-10-30)"
+__version__ = "3.3 (2021-10-30)"
 __copyright__ = """recode v%(version)s
 
 Copyright (c) 2019-2020 by Yuhao Gu. All rights reserved.
 """
 
 import os
+import re
 from collections import Counter
-from fnmatch import fnmatch
 from typing import Union
 
 import chardet
@@ -83,12 +83,12 @@ def atomic_rewrite(path: str, s: str, encoding, newline):
 @click.option(
     "--include",
     help="Only include files that matches this pattern.",
-    default="*",
+    default=".*",
 )
 @click.option(
     "--exclude",
     help="Exclude files that matches this pattern.",
-    default="",
+    default="^$",
 )
 @click.option(
     "-f",
@@ -146,30 +146,39 @@ def cli(
     4. 保持原编码，只修改行尾
         recode file dir --eof=CR
     \b
-    5. 模式包含与排除，同时匹配时，优先选择排除
-        recode -r dir1 dir2 --include "a*" --exclude "*.cpp"
+    5. 正则模式包含与排除，同时匹配时，优先选择排除
+        recode -r dir1 dir2 --include "a.*" --exclude ".*.cpp"
     """
 
-    def match(name):
-        return not fnmatch(name, exclude) and fnmatch(name, include)
+    include = re.compile(include)
+    exclude = re.compile(exclude)
+
+    def match(path):
+        name = os.path.basename(path)
+        return (
+            include.match(name)
+            and not exclude.match(name)
+            and not exclude.search(path)
+        )
 
     def gen_matched_files():
         for path in pathes:
             if os.path.isfile(path):
-                if match(os.path.basename(path)):
+                if match(path):
                     yield path
 
             elif os.path.isdir(path):
                 for name in os.listdir(path):
                     full_path = os.path.join(path, name)
-                    if match(name) and os.path.isfile(full_path):
+                    if match(full_path) and os.path.isfile(full_path):
                         yield full_path
 
         for dir in recursive:
             for path, _, files in os.walk(dir):
                 for file in files:
-                    if match(file):
-                        yield os.path.join(path, file)
+                    full_path = os.path.join(path, file)
+                    if match(full_path):
+                        yield full_path
         return
 
     if not to and not eof:
@@ -188,7 +197,9 @@ def cli(
                     click.style(f"[{translate_newlines(eof)}]", fg="green")
                 ] += 1
 
-                result = click.style(f"[{encoding}, {translate_newlines(eof)}]", fg="green")
+                result = click.style(
+                    f"[{encoding}, {translate_newlines(eof)}]", fg="green"
+                )
 
             except BaseException as e:
                 result = click.style(f"[{type(e).__name__}]", bg="red")
@@ -213,6 +224,7 @@ def cli(
     # 定制执行过程
     if to and eof:
         # 全功能模式
+        to = to.upper()
         newline = {"CR": "\r", "LF": "\n", "CRLF": "\r\n"}[eof]
 
         def process(path):
@@ -249,6 +261,8 @@ def cli(
             )
 
     elif to:
+        to = to.upper()
+
         # 只重新编码
         def process(path):
             if not force_encoding:
@@ -304,22 +318,5 @@ def cli(
     return
 
 
-def test():
-    from click.testing import CliRunner
-
-    runner = CliRunner()
-    result = runner.invoke(
-        cli,
-        [
-            "-r",
-            r"C:\Users\GuYuhao\Desktop\2",
-            # "-t",
-            # "gbk",
-        ],
-    )
-    return result
-
-
 if __name__ == "__main__":
     cli()
-    # print(test())
